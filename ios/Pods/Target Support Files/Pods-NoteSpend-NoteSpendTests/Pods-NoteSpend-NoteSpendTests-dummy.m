d22cd67adbe9dba05c2ce1bf14d5d4c451a2ead9
Diep Quang Nghia <nghiadqdev@gmail.com>
2022-08-20 14:44:07 +0700

    first commit

---
+++ ios/Pods/Target Support Files/Pods-NoteSpend-NoteSpendTests/Pods-NoteSpend-NoteSpendTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NoteSpend_NoteSpendTests : NSObject
+@end
+@implementation PodsDummy_Pods_NoteSpend_NoteSpendTests
+@end
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeIndex = () => {
+    return (
+        <View>
+            <Text>HomeIndex</Text>
+        </View>
+    )
+}
+
+export default HomeIndex
+
+const styles = StyleSheet.create({})
